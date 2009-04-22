fe1b10a593314d922f09045e665c2dafa056ea6b
metebilgin <metebilgin48@gmail.com>
2009-04-22 10:21:32 +0000

    v2

--- index.py
+++ index.py
@@ -5,10 +5,10 @@ def index():
    s =template_page.page_header()+ """\
 <body>
 <form method="post" action="./show.py">
-<p>ldap host: <input type="text" name="host_name"></p>
-<p>username: <input type="text" name="word"></p>
+<p>ldap host: <input type="text" name="host_name" value="localhost"></p>
+<p>username: <input type="text" name="word" value="cn=Manager,dc=my-domain,dc=com" ></p>
 <p>password: <input type="password" name="pas"></p>
-<p>base_dn: <input type="text" name="base_dn"></p>
+<p>base_dn: <input type="text" name="base_dn" value="dc=my-domain,dc=com"></p>
 <p>language: 
 <select NAME="language">
 <option value="tr"> Turkçe</option>
--- show.py
+++ show.py
@@ -20,34 +20,47 @@ def index(req):
         ldap_server.protocol_version = ldap.VERSION3
         ldap_server.bind_s(server[0],server[1])
         ldap_server.unbind()
-        return template_page.page_header()+"""<link rel=stylesheet href="deneme.css" type="text/css" media=screen><script language="JavaScript">
+        return template_page.page_header()+"""<link rel="stylesheet" href="deneme.css" type="text/css" media="screen"/>
+        <link rel="SHORTCUT ICON" href="img/icon.ico"/>
+        <script type="text/javascript">
         function a(){
-        document.getElementById("main_frame").src= "./list.py";
+        document.getElementById("main_frame").data= "./list.py";
         }
         function b(){
-        document.getElementById("main_frame").src= "./delete_user.py";
+        document.getElementById("main_frame").data= "./delete_user.py";
         }
         function c(){
-        document.getElementById("main_frame").src= "./add.py";
+        document.getElementById("main_frame").data= "./add.py";
         }
         function d(){
-        document.getElementById("main_frame").src= "./search.py";
+        document.getElementById("main_frame").data= "./search.py";
         }
         function e(){
-        document.getElementById("main_frame").src= "./server_info.py";
+        document.getElementById("main_frame").data= "./export.py";
         }
-        </script></head><body>
+        function f()
+        {
+        document.getElementById("main_frame").data="./file_input.py/form"
+        }
+        </script>
+        <title> pyldapadmin</title>
+        </head>
+        <body>
        <div id="sayfa"><div id="baslik"></div><div id="anabolge"><div id="yanmenu">
-       <p><a href="#" onclick="a()" ><img src="./img/list.png"><br>"""+ _('List')+"""</a></p>
-       <p><a href="#" onclick="b()"><img src="./img/trash.png"><br>"""+_(' delete')+"""</a></p>
-       <p><a href="#" onclick="c()"><img src="./img/user.png"><br>"""+_('add user')+"""</a></p>
-       <p><a href="#" onclick="d()"><img src="./img/search.png"><br>"""+_('search')+""" </a></p>
-       <p><a href="./server_info.py" onclick=""><img src="./img/exit.png"><br>"""+_('logout')+"""</a></p></div>
+       <p><a href="#" onclick="a()" ><img src="./img/list.png" alt=""/><br/>"""+ _('List')+"""</a></p>
+       <p><a href="#" onclick="b()"><img src="./img/trash.png" alt=""/><br/>"""+_(' delete')+"""</a></p>
+       <p><a href="#" onclick="c()"><img src="./img/user.png" alt=""/><br/>"""+_('add user')+"""</a></p>
+       <p><a href="#" onclick="d()"><img src="./img/search.png" alt=""/><br/>"""+_('search')+""" </a></p>
+       <p><a href="#" onclick="e()"><img src="./img/download.png" alt=""/><br/>"""+_('export')+""" </a></p>
+       <p><a href="#" onclick="f()"><img src="./img/import.png" alt=""/><br/>"""+_('import')+""" </a></p>
+
+       <p><a href="./server_info.py" onclick=""><img src="./img/exit.png" alt=""/><br/>"""+_('logout')+"""</a></p></div>
        <div id="icerik">
-           <iframe id="main_frame" src=""  width="100%" height="400px" scrolling="auto" frameborder="0" marginwidth="0" marginheight="0"></iframe>
+           <object id="main_frame" data="login.py"  width="100%" height="400px"></object>
        </div></div><div id="altlik"></div></div>
                </body></html>"""
-
+    except UnboundLocalError,e:
+        return template_page.page_header()+'<title>error page</title></head><body><p><img alt="error" src="./img/error.png"/>there is some problem please<a href="./">try</a>again.</p></body></html>'
     except:
         page = template_page.page_header()+'<title>'+_('error page')+'</title></head><body><p><img alt="error" src="./img/error.png"/>'+ _('there is some problem please')+' <a href="./">'+_('try')+'</a> '+_('again')+'.</p></body></html>'
         server_info.index(req)
